d66e0700910c633a78a3798288ccfb810368d389
IsraelJorge <israeljorge2000@gmail.com>
2024-05-05 15:15:26 -0300

    feat: add promo banner to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { CategoryList } from '@/components/category-list';
 import { Header } from '@/components/header';
 import { Search } from '@/components/search';
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -12,6 +13,18 @@ export default function Home() {
 
       <div className="px-5 pt-6">
         <CategoryList />
+      </div>
+
+      <div className="px-5 pt-6">
+        <Image
+          src="/images/promo-banner-01.svg"
+          alt="Até 30% de desconto em pizzas!"
+          width={0}
+          height={0}
+          className="h-auto w-full"
+          sizes="100vw"
+          quality={100}
+        />
       </div>
     </div>
   );
