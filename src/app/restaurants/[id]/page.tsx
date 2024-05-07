6a7c6ae7e686866c1c05f28533b1f384ca88dfe7
IsraelJorge <israeljorge2000@gmail.com>
2024-05-06 22:01:31 -0300

    feat: add restaurant page

--- src/app/restaurants/[id]/page.tsx
+++ src/app/restaurants/[id]/page.tsx
@@ -4,6 +4,7 @@ import { RestaurantImage } from './components/restaurant-image'
 import { Image } from '@/components/image'
 import { BadgeStar } from '@/components/badge-star'
 import { DeliveryCard } from '@/components/delivery-card'
+import { ProductList } from '@/components/product-list'
 
 export type RestaurantPageProps = {
   params: {
@@ -17,6 +18,39 @@ export default async function RestaurantPage({
   const restaurant = await db.restaurant.findUnique({
     where: {
       id,
+    },
+    include: {
+      categories: {
+        orderBy: {
+          createdAt: 'desc',
+        },
+        select: {
+          id: true,
+          name: true,
+          products: {
+            where: {
+              restaurantId: id,
+            },
+            include: {
+              restaurant: {
+                select: {
+                  name: true,
+                },
+              },
+            },
+          },
+        },
+      },
+      products: {
+        include: {
+          restaurant: {
+            select: {
+              name: true,
+            },
+          },
+        },
+        take: 10,
+      },
     },
   })
 
@@ -45,6 +79,31 @@ export default async function RestaurantPage({
       <div className="mx-5 mt-6 ">
         <DeliveryCard restaurant={restaurant} />
       </div>
+
+      <div className="scroll-hidden mt-3 flex gap-4 px-5">
+        {restaurant.categories.map((category) => (
+          <div
+            key={category.id}
+            className="min-w-[167px] rounded-sm bg-[#F4F4F4] text-center"
+          >
+            <span className="text-xs font-semibold text-muted-foreground">
+              {category.name}
+            </span>
+          </div>
+        ))}
+      </div>
+
+      <div className="mt-6 space-y-4">
+        <h2 className="px-5 font-semibold">Mais Pedidos</h2>
+        <ProductList products={restaurant.products} />
+      </div>
+
+      {restaurant.categories.map((category) => (
+        <div key={category.id} className="mt-6 space-y-4">
+          <h2 className="px-5 font-semibold">{category.name}</h2>
+          <ProductList products={category.products} />
+        </div>
+      ))}
     </div>
   )
 }
--- src/utils/ui/Routes.ts
+++ src/utils/ui/Routes.ts
@@ -1,3 +1,4 @@
 export const Routes = {
   product: (id: string) => `/products/${id}`,
+  restaurant: (id: string) => `/restaurants/${id}`,
 }
