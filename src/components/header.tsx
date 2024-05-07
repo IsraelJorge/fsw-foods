1dba22392db34e8e2bc0711842c3ac685f97ea10
IsraelJorge <israeljorge2000@gmail.com>
2024-05-06 22:26:35 -0300

    feat: add link to home in logo header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,17 +1,20 @@
 import { Image } from './image'
 import { Button } from './ui/button'
 import { Icon } from './icon'
+import Link from 'next/link'
 
 export function Header() {
   return (
     <header className="flex justify-between px-5 pt-6">
-      <Image
-        src="/images/logo.svg"
-        alt="FSW Foods"
-        width={100}
-        height={30}
-        className="h-[30px] w-[100px]"
-      />
+      <Link href="/">
+        <Image
+          src="/images/logo.svg"
+          alt="FSW Foods"
+          width={100}
+          height={30}
+          className="h-[30px] w-[100px]"
+        />
+      </Link>
       <Button
         size="icon"
         variant="outline"
